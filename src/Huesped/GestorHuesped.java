791fd76591152e1c5372f6b246dae4a091a2cfef
Marcos <Marcos@DESKTOP-VI95HPB>
2025-10-21 15:48:06 -0300

    Comentarios en GestorHuesped

--- src/Huesped/GestorHuesped.java
+++ src/Huesped/GestorHuesped.java
@@ -20,16 +20,21 @@ public class GestorHuesped {
         //▪ Número de documento.
         //puede no poner ninguno, como algunos. Manejar esto.
         // Presiona buscar (hacer boton o algo por el estilo)
+        //Según el diagrama de secuencia el ingreso y presionar buscar se hace en la pantalla
+        // y luego se le pasan los datos ingresados al gestor, y se maneja lo de que no 
+        // ingrese ningún dato o algunos.
 
-        //dao busca con los criterios pasados los huespedes, devuelve dtos?
+        //dao busca con los criterios pasados los huespedes, devuelve dtos? -> Esto va en la
+        // parte de DAO, el gestor nomás le pasa los datos y luego recibe los resultados
         //ver si existe concordancia, si no pasar al cu11 y finalizar este cu.
         //si encuentra seguir
         // presentar los datos de los dto encontrados en pantalla de la manera correcta
         // manera: Esta lista contiene como columnas los datos mencionados en el paso 2.
-        //Seleccionar una persona de alguna manera
-        //Presiona siguiente.
+        //Seleccionar una persona de alguna manera -> no lo hace el gestor
+        //Presiona siguiente. -> no lo hace el gestor
         //Chequear si no selecciono nadie y apreto siguiente, ir al cu11.
-        //si apreto bien ir al cu10. y terminar.
+        //si apreto bien ir al cu10. y terminar. -> Segun el diagrama de secuencia
+        // esto tampoco lo hace el gestor
     }
 
     public boolean darAltaHuesped(){
@@ -37,28 +42,29 @@ public class GestorHuesped {
         //una vez ejecutado el buscarHuesped(), se puede ahora ejecutar este
 
         //pantalla presenta datos, inputs (algunos obligariorios y otros no)
-        // y botones Siguiente y Cancelar
+        // y botones Siguiente y Cancelar -> no lo hace el gestor
 
             //no ingresa todos los obligatorios y toca siguiente
             //Mostramos un mensaje de error detallando cada una de las omisiones. (podemos concatenar un string si es que falla)
-            //vuelve a pedir ingreso de datos
+            //vuelve a pedir ingreso de datos ->Puede haber otro error de validación
 
-            //ya existe el dni, mostrar mensaje CUIDADO ...
-            //Botones aceptar igualmente o corregir
-                //Aceptar igualmente lleva a seguir por el flujo normal
-                //Corregir vuelve a pedir el ingreso del dni solamente
+            //ya existe el huesped y por lo tanto el dni, mostrar mensaje CUIDADO ... -> según el 
+            // diagrama de secuencia lo hace la pantalla
+            //Botones aceptar igualmente o corregir -> pantalla
+                //Aceptar igualmente lleva a seguir por el flujo normal -> crea el huesped y la direccion
+                //Corregir vuelve a pedir el ingreso del dni solamente -> pantalla
 
-            //Cancelar
+            //Cancelar -> segun el diagrama de secuencia lo hace la pantalla
             //mensaje ¿Desea cancelar el alta del huésped? Y los botones Si y No.
                 //Si, termina el metodo
                 //no, regresa al principio del metodo
 
-        //Registramos el huesped, persistimos en bdd
+        //Registramos el huesped, persistimos en bdd -> lo hace el gestor o el DAO?
         //“El huésped nombres y apellido ha sido satisfactoriamente cargado al sistema. ¿Desea cargar otro?
         //Botones Si No
         //No y termina
         //Si y volves al principio de todo
-
+        // Segun el diagrama de secuencia no lo hace el gestor
     }
 
     public boolean modificarHuesped(){
