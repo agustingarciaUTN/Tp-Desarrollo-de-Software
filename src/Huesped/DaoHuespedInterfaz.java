560ab7223442a3844e7777d287679dd2fa73cf14
Marcos <Marcos@DESKTOP-VI95HPB>
2025-10-23 20:34:22 -0300

    Correccion interfaz DAO

--- src/Huesped/DaoHuespedInterfaz.java
+++ src/Huesped/DaoHuespedInterfaz.java
@@ -3,10 +3,11 @@ package Huesped;
 import Dominio.Huesped;
 
 public interface DaoHuespedInterfaz {
-    boolean CrearHuesped(DtoHuesped dto);
-    boolean ModificarHuesped(int idUsuario);
-    boolean EliminarHuesped(int idUsuario);
-    Huesped ObtenerHuesped(int idUsuario);
+    boolean crearHuesped(DtoHuesped dto);
+    boolean modificarHuesped(int idUsuario);
+    boolean eliminarHuesped(int idUsuario);
+    List<DtoHuesped> obtenerTodosLosHuespedes ();
+    List<DtoHuesped> obtenerHuespedesPorCriterio(DtoHuesped criterios);
 }
 //queremos tener diferentes metodos para devolver por ej una lista de dto?
 //tenemos que controlar excepciones
