452e54f6270f6d520e054d2663d82cd5bc8bb912
AgustinGarciaUTN <aggarcia@frsf.utn.edu.ar>
2025-10-24 17:58:01 -0300

    Instale bien (creo) el drive de la bdd.

--- src/BaseDedatos/Coneccion.java
+++ src/BaseDedatos/Coneccion.java
@@ -6,10 +6,10 @@ import java.sql.SQLException;
 
 public final class Coneccion {
     
-    private static final String DRIVER="";
-    private static final String USUARIO="";
-    private static final String CONTRASENIA="";
-    private static final String URL="";
+    private static final String DRIVER="jdbc:postgresql://ep-red-truth-aczpiy6y-pooler.sa-east-1.aws.neon.tech/neondb?user=neondb_owner&password=npg_HzBE38OvfPXs&sslmode=require&channelBinding=require";
+    private static final String USUARIO="neondb_owner";
+    private static final String CONTRASENIA="npg_QufXyUrW7aL9";
+    private static final String URL="https://console.neon.tech/app/projects/wild-queen-99662535?branchId=br-damp-resonance-ac1sm2fv&database=neondb";
     
     private Coneccion (){
         
