216164864500d07f0b42073b9b1b1b75ff408b3d
Marcos <Marcos@DESKTOP-VI95HPB>
2025-10-28 17:28:07 -0300

    Ultimo

--- src/Huesped/DaoHuesped.java
+++ src/Huesped/DaoHuesped.java
@@ -13,22 +13,6 @@ import java.sql.SQLException;
 
 public class DaoHuesped implements DaoHuespedInterfaz {
 
-    //estos son los atributos que necesita la clase para conectarse a la DB
-    private static final String DB_URL =;
-    private static final String USER =;
-    private static final String PASS =;
-
-    //metodo para realizarla coneccion a la DB
-    private Connection getConnection() throws SQLException{
-        return DriverManager.getConnection(DB_URL, USER, PASS);
-    }
-
-    public boolean CrearHuesped(DtoHuesped dto){}
-    public boolean ModificarHuesped(int idUsuario){}
-    public boolean EliminarHuesped(int idUsuario){}
-    public DtoHuesped ObtenerHuesped(int idUsuario){}
-
-
     public boolean crearHuesped(DtoHuesped dto){
      
     }
